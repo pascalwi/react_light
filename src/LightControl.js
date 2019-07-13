6d3cd938c1f3298599ca69dc499c080668837082
pascalwi <pascal.wilzki@gmail.com>
2019-07-13 14:29:06 +0200

    node server not connected to react

--- src/LightControl.js
+++ src/LightControl.js
@@ -4,28 +4,8 @@ import React from 'react';
 
 class LightControl extends React.Component {
 
-  
-
-handleSubmit = e => {
-        
-    var Gpio = require('onoff').Gpio
-    var led = new Gpio(17, 'out')
-    led.writeSync(1)
-    console.log("hi")
-    e.preventDefault();
-}
-
-handleClick = e =>{
-    console.log("Click")
-    var Gpio = require('onoff').Gpio
-    var led = new Gpio(17, 'out')
-    led.writeSync(1)  
-}
-
 render(){
-    return(<form>    
-    <button onClick={this.handleClick}>haldlo</button>    
-    </form>)    
+    return(<h1>hello</h1>)    
     };
 }
 
